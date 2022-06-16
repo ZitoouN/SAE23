68d8680c123b6f060fa017ad6b59cf5bf4e78f03
TheWilli67 <william.hertrich@numericable.fr>
2022-06-16 11:45:25 +0200

    modif

--- ludotheque/ludotheque/settings.py
+++ ludotheque/ludotheque/settings.py
@@ -116,8 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/csstemplates/'
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
